2083e26f9a0bd5256c05b999f391aac94e34d2b5
hwookim <gusdn9754@gmail.com>
2020-03-06 12:28:03 +0900

    feat : Cars.createCars 추가
     List<Car>를 반환 해 Cars.cars 초기화

--- src/main/java/racing/Cars.java
+++ src/main/java/racing/Cars.java
@@ -3,13 +3,20 @@ package racing;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Cars {
     List<Car> cars;
 
     public Cars(List<String> names) {
         validateNameDuplication(names);
+        this.cars = createCars(names);
+    }
 
+    private List<Car> createCars(List<String> names) {
+        return names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     private void validateNameDuplication(List<String> names) {
--- src/test/java/racing/CarsTest.java
+++ src/test/java/racing/CarsTest.java
@@ -6,9 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsTest {
+    @Test
+    @DisplayName("Cars 생성 테스트")
+    void initTest() {
+        List<String> input = Arrays.asList("A", "B", "C");
+
+        assertThatCode(() -> new Cars(input))
+                .doesNotThrowAnyException();
+    }
+
     @Test
     @DisplayName("이름 중복 체크")
     void validateNameDuplication() {
